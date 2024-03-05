d7fae569a8d824c3038e1cd766699232fa046533
j-krnjak <162152330+j-krnjak@users.noreply.github.com>
2024-03-05 08:47:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 #from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -27,10 +27,13 @@ ingredients_list=st.multiselect(
 
 if ingredients_list:
     ingredients_string=''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #st.text(fruityvice_response.json())
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('"""+ingredients_string+"""', '"""+ name_on_order +"""')"""
     st.write(my_insert_stmt)
@@ -42,7 +45,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
 fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
